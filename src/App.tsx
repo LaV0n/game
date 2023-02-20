9fb9b34c4c8df8037ded3df4681dfd008f530be6
LaV0n <leonidmedica@gmail.com>
2023-02-20 15:12:16 +0100

    feat :start add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Canvas} from "./features/Canvas/Canvas";
 
 function App() {
+
+    class InputHandler{
+
+    }
+    class ProjectTitle{}
+    class Particle{}
+    class Plqyer{
+        game:any;
+        width:number;
+        height:number;
+        x:number;
+        y:number;
+        speedY:number
+        constructor(game:any) {
+            this.game=game;
+            this.width=120;
+            this.height=190;
+            this.x=20;
+            this.y=100;
+            this.speedY=0.2;
+        }
+        updade(){
+            this.y +=this.speedY;
+        }
+        draw(context:CanvasRenderingContext2D){
+            context.fillRect(this.x,this.y,this.width, this.height);
+        }
+    }
+    class Enemy{}
+    class Layer{}
+    class Backfround{}
+    class UI{}
+    class Game{
+        width:number;
+        height:number;
+        player: any
+        constructor(width:number,height:number) {
+            this.width=width;
+            this.height=height;
+            this.player= new Plqyer(this);
+        }
+        update(){
+            this.player.updade();
+        }
+        draw(context:any){
+            this.player.draw(context)
+        }
+    }
+    const game= new Game(window.innerWidth,400)
+
+    function animate(ctx:CanvasRenderingContext2D){
+        game.update();
+        game.draw(ctx)
+        requestAnimationFrame(()=>animate(ctx))
+        ctx.clearRect(0,0,500,500)
+    }
+    const drawArt=(ctx:CanvasRenderingContext2D)=> {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(0, 0, 120, 100)
+        ctx.strokeRect(120,100,50,50)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas width={window.innerWidth} height={400} draw={drawArt}/>
+      <Canvas width={window.innerWidth} height={400} draw={animate}/>
     </div>
   );
 }
