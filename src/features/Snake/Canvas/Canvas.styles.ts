c2e0fe489b4303472559bffe93b7813c271a1914
LaV0n <leanid.dev@gmail.com>
2023-03-03 11:07:00 +0100

    feat: add style and store

--- src/features/Snake/Canvas/Canvas.styles.ts
+++ src/features/Snake/Canvas/Canvas.styles.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Canvas=styled.canvas`
-  border: 10px solid black;
+  border: 10px double #989797;
   width: 800px;
   height: 400px;
+  background-color: rgba(2, 2, 2, 0.7);
 `
--- src/features/Snake/Snake.tsx
+++ src/features/Snake/Snake.tsx
@@ -5,17 +5,19 @@ import homeIcon from "../../assets/icon/icons8-home-page-64.png";
 import {Canvas} from "./Canvas/Canvas";
 import {useGameLogic} from "./useGameLogic";
 import {draw} from "./draw/draw";
+import wasdIcon from '../../assets/icon/icons8-клавиатура-64.png'
 
 export enum GameState {
     RUNNING,
     GAME_OVER,
-    PAUSE
+    PAUSE,
+    WAITING
 }
 
 export const Snake = () => {
 
     const canvasRef = useRef<HTMLCanvasElement>(null)
-    const [gameState, setGameState] = useState<GameState>(GameState.RUNNING)
+    const [gameState, setGameState] = useState<GameState>(GameState.WAITING)
 
     const onGameOver = () => setGameState(GameState.GAME_OVER)
 
@@ -31,7 +33,7 @@ export const Snake = () => {
     }
 
     const playAgainHandler = () => {
-        setGameState(GameState.RUNNING)
+        setGameState(GameState.WAITING)
         resetGameState()
     }
     const pauseButtonHandler=()=>{
@@ -45,14 +47,33 @@ export const Snake = () => {
             <Link to="/" className={styles.homeLink}>
                 <img src={homeIcon} alt="0"/>
             </Link>
-            <Canvas ref={canvasRef} draw={drawGame}/>
-            {gameState === GameState.GAME_OVER
-                ? (<button onClick={playAgainHandler}>Play Again</button>)
-                : (<button onClick={pauseButtonHandler}>
-                    {gameState===GameState.RUNNING
-                    ?'pause'
-                    :'play'}
-                </button>)}
+            <div className={styles.gameBlock}>
+                <div className={styles.storeBlock}>
+                    <div className={styles.descriptionBlock}>
+                        STORE:
+                        <p className={styles.title}>{(snakeBody.length-1)*10}</p>
+                    </div>
+                    {gameState === GameState.GAME_OVER
+                        ? (<button onClick={playAgainHandler}>Play Again</button>)
+                        : (<button onClick={pauseButtonHandler}
+                        >
+                            {gameState===GameState.RUNNING
+                                ?'pause'
+                                :'play'}
+                        </button>)}
+                    <div className={styles.descriptionBlock}>
+                        <p className={styles.title}>For action use:</p>
+                        <img src={wasdIcon} alt="0" className={styles.wasdIcon}/>
+                    </div>
+                </div>
+                <Canvas ref={canvasRef} draw={drawGame}/>
+                {gameState === GameState.GAME_OVER &&
+                <div className={styles.endGameTitle}>
+                    GAME OVER
+                </div>
+                }
+            </div>
+
         </div>
     );
 };
